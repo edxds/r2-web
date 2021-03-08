c8447dd30ff545a3508b7aee437adb56dc22a341
Eduardo Souza <edux404@gmail.com>
2021-03-07 21:09:35 -0300

    feat(components): add BackButton component

---
+++ src/components/Button/BackButton.tsx
@@ -0,0 +1,33 @@
+import { ElementType, forwardRef } from 'react';
+
+import { ReactComponent as BackIcon } from '@r2/assets/icons/back.svg';
+
+import { Button, ButtonProps } from './Button';
+
+export interface BackButtonOwnProps {
+  title?: string;
+}
+
+export type BackButtonProps<E extends ElementType> = BackButtonOwnProps & ButtonProps<E>;
+
+const defaultElement = 'button';
+
+export const BackButton: <E extends ElementType = typeof defaultElement>(
+  props: BackButtonProps<E>,
+) => JSX.Element | null = forwardRef(
+  <E extends ElementType = typeof defaultElement>(
+    {
+      title = 'Voltar',
+      size = 'md',
+      color = 'primary',
+      variant = 'text',
+      props,
+    }: BackButtonProps<E>,
+    ref: typeof props.ref,
+  ) => (
+    <Button ref={ref} size={size} color={color} variant={variant} {...props}>
+      <BackIcon className="fill-current icon" />
+      <span>{title}</span>
+    </Button>
+  ),
+);
