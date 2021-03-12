2e0fb108cb4db2698dcfcea024621ba140867ab3
Eduardo Souza <edux404@gmail.com>
2021-03-12 00:07:58 -0300

    feat(text-field): add error state

--- src/components/Fields/TextField.tsx
+++ src/components/Fields/TextField.tsx
@@ -1,17 +1,21 @@
 import clsx from 'clsx';
 import { ComponentPropsWithoutRef } from 'react';
 
-export interface TextFieldProps extends ComponentPropsWithoutRef<'input'> {}
+export interface TextFieldProps extends ComponentPropsWithoutRef<'input'> {
+  hasError?: boolean;
+}
 
-export function TextField({ className, ...props }: TextFieldProps) {
+export function TextField({ className, hasError, ...props }: TextFieldProps) {
   return (
     <input
       type="text"
       className={clsx(
         'text-base text-gray-800',
         'rounded-xl border-gray-300 outline-none',
-        'focus:shadow-md focus:border-brand',
+        'focus:shadow-md focus:ring-1',
         'transition-all',
+        hasError && 'border-red-600 focus:ring-red-600 focus:border-red-600',
+        !hasError && 'focus:border-brand focus:ring-brand',
         className,
       )}
       {...props}
