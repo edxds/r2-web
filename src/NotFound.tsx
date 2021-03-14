c8d97316ecf4fa69df77db7a0fb887eda78065be
Eduardo Souza <edux404@gmail.com>
2021-03-14 03:37:03 -0300

    feat(not-found): make fluid

--- src/NotFound.tsx
+++ src/NotFound.tsx
@@ -5,13 +5,9 @@ import { Button } from './components/Button';
 
 export function NotFound() {
   return (
-    <div className="bg-gray-50">
+    <div className="flex flex-col flex-1 bg-gray-50">
       <div
-        className={clsx(
-          'max-w-screen-md mx-auto min-h-screen',
-          'flex flex-col justify-center',
-          'p-6',
-        )}
+        className={clsx('max-w-screen-md mx-auto flex-1', 'flex flex-col justify-center', 'p-6')}
       >
         <div className="space-y-4 mb-16 md:mb-24">
           <h1 className="font-black text-4xl text-gray-800">Perdido?</h1>
